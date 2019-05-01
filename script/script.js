91332b3508a2991dd280eedb55e2c6f724cc0b07
MacoChave <201020831@ingenieria.usac.edu.gt>
2019-05-01 00:37:57 -0600

    Drawer menu implementado

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let btn_drawer_menu = document.querySelectorAll('.drawer-menu')[0];
+let btn_action_menu = document.querySelectorAll('.action-menu')[0];
+
+btn_drawer_menu.addEventListener('click', () => {
+    let menu = document.querySelectorAll('.menu')[0];
+    menu.classList.toggle('hide-menu');
+    console.log('Drawer button pressed');
+});
+
+btn_action_menu.addEventListener('click', () => {
+    console.log('Action button pressed');
+});
